bf8b374135f850ec36cc623b27abd43a5b8cdd67
christopherdavi <67648870+christopherdavi@users.noreply.github.com>
2020-08-13 09:59:11 -0500

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    internal class Calculadora
+    {
+        internal static double[,] CrearMatriz(int filasMatriz, int columnaMatriz)
+        {
+            double[,] matriz = new double[filasMatriz, columnaMatriz];
+
+            var generador = new Random();
+
+            for (int i = 0; i < filasMatriz; i++)
+            {
+                for (int j = 0; j < columnaMatriz; j++)
+                {
+                    matriz[i, j] = generador.Next(10,90);
+                }
+            }
+            return matriz;
+        }
+        internal double[,] Multiplicar(double[,] matrizA, double[,] matrizB)
+        {
+            int filas = matrizA.GetLength(0);
+            int columnas = matrizB.GetLength(1);
+            double[,] matrizR = new double[filas,columnas];
+
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < matrizA.GetLength(1);k++);
+                    temp += matrizA[i, k] * matrizB[k, j];
+                    matrizR[i, j] = temp;
+                }
+            }
+            return matrizR;
+        }
+
+        internal double[,] MultiplicarParalelo(double[,] matrizA, double[,] matrizB)
+        {
+            int filas = matrizA.GetLength(0);
+            int columnas = matrizB.GetLength(1);
+            
+            double[,] matrizR = new double[filas, columnas];
+            //for (int i = 0; i < filas; i++)
+                Parallel.For(0, filas, i =>
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    double temp = 0;
+                    for (int k = 0; k < matrizA.GetLength(1); k++) ;
+                    temp += matrizA[i, k] * matrizB[k, j];
+                    matrizR[i, j] = temp;
+                }
+            });
+            return matrizR;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Matrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int filasMatrizA = 100;
+            int columnaMatrizA = 90;
+            int filasMatrizB = columnaMatrizA;
+            int columnasMatrizB = 6;
+
+            double[,] matrizA = Calculadora.CrearMatriz(filasMatrizA, columnaMatrizA);
+            double[,] matrizB = Calculadora.CrearMatriz(filasMatrizB, columnasMatrizB);
+
+            Calculadora calculadora = new Calculadora();
+            Stopwatch stopWatch = new Stopwatch();
+            Stopwatch.StartNew();
+            double[,] matriz = calculadora.MultiplicarParalelo(matrizA, matrizB);
+            Stopwatch.stop();
+            var tecks = stopWatch.ElapsedTicks;
+            Console.WriteLine("Se demoro {tecks}");
+
+
+            Stopwatch stopWatch1 = new Stopwatch();
+
+            stopWatch1.Start();
+            double[,] matrizAxBPrima = calculadora.Multiplicar(matrizA, matrizB);
+            stopWatch1.stop();
+            tecks = stopWatch.ElapsedTicks;
+            Console.WriteLine("Se demoro {tecks}");
+
+            
+            //Imprimirmatriz(matriz);
+            //Console.WriteLine("\nPrima");
+            //Imprimirmatriz(matrizAxBPrima);
+        }
+
+        private static void Imprimirmatriz(double[,] matrizAxB)
+        {
+            for (int i = 0; i < matrizAxB.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizAxB.GetLength(1); j++)
+                {
+                    Console.Write(matrizAxB[i, j] + " ");
+                }
+
+            }
+        }
+    }
+}
